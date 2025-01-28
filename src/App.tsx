428afefee1215f5799c4b987f34626ea79502546
Anxo55 <anxo.campos260204@gmail.com>
2025-01-29 00:02:20 +0100

    menu-links

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,24 @@ import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import UsersProfile from "./pages/UsersProfile";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-    <NavBar/>
-    <div className="container mx-auto px-8 py-30">
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/usersPRofile" element={<UsersProfile/>}/>
-      </Routes>
-    </div>
+      <div className="flex flex-col min-h-screen">
+        <NavBar />
+        <div className="flex-grow container mx-auto px-8 py-30">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/usersProfile" element={<UsersProfile />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
