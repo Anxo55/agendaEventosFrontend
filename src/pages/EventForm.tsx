d0d90697f8dbf5ea33071d41cb21379b90d249fb
Anxo55 <anxo.campos260204@gmail.com>
2025-02-19 21:54:13 +0100

    volver-del-formulario

--- src/pages/EventForm.tsx
+++ src/pages/EventForm.tsx
@@ -1,18 +1,20 @@
-import { useState } from 'react';
-import { createEvent } from '../services/events.service';
-import toast from 'react-hot-toast';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { createEvent } from "../services/events.service";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function EventForm() {
   const [event, setEvent] = useState({
-    title: '',
-    description: '',
-    location: '',
-    imageUrl: ''
+    title: "",
+    description: "",
+    location: "",
+    imageUrl: "",
   });
   const navigate = useNavigate();
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
     setEvent({ ...event, [name]: value });
   };
@@ -21,17 +23,19 @@ export default function EventForm() {
     e.preventDefault();
     try {
       await createEvent(event);
-      toast.success('Evento creado con éxito');
+      toast.success("Evento creado con éxito");
       navigate("/eventsList");
     } catch (error) {
-      console.error('Error al crear el evento:', error);
-      toast.error('Error al crear el evento');
+      console.error("Error al crear el evento:", error);
+      toast.error("Error al crear el evento");
     }
   };
 
   return (
     <div className="max-w-3xl mx-auto p-4  rounded-xl shadow-md">
-      <h2 className="text-2xl font-bold text-center mb-6 text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Crear Nuevo Evento</h2>
+      <h2 className="text-2xl font-bold text-center mb-6 text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+        Crear Nuevo Evento
+      </h2>
       <form onSubmit={handleSubmit} className="space-y-4">
         <input
           type="text"
@@ -63,12 +67,16 @@ export default function EventForm() {
           onChange={handleChange}
           className="w-full p-2 border rounded-2xl text-white placeholder:text-white"
         />
-        <button type="submit" className="w-full bg-green-600 text-white py-2 rounded-2xl border-2 hover:bg-green-700">
+        <button
+          type="submit"
+          className="w-full bg-green-600 text-white py-2 rounded-2xl border-2 hover:bg-green-700"
+        >
           Crear Evento
         </button>
       </form>
+      <Link to="/eventsList"
+      className="text-2xl bg-blue-600 hover:bg-blue-700 rounded-2xl border-2 border-white text-white m-4  grid place-items-center"
+      >Volver</Link>
     </div>
   );
 }
-
-
